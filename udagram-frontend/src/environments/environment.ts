031f04f821d32160f36c4f2f6c03fb896482754b
HaoVA <hi@haova.me>
2023-12-11 12:21:27 +0700

    update deployment

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a11b3e4cc7c104192bff66cbde78c41d-1902260030.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
